219ed8f02219a9b787278206dc9ed77742705f44
Tom Shields <tom@eriven.com>
2022-09-09 14:14:21 -0700

    correct coupon balance for withdraws and transfers

--- contracts/redemption/ERC4626Coupon.sol
+++ contracts/redemption/ERC4626Coupon.sol
@@ -6,8 +6,6 @@ import "../ERC4626SuiteContext.sol";
 
 /// @notice Implement payment of coupons for ERC4626 vaults. Can also be used for dividends.
 
-// TODO consider making this clonable and therefore initializable
-
 abstract contract ERC4626Coupon is ERC4626SuiteContext {
     using Math for uint256;
 
@@ -26,15 +24,6 @@ abstract contract ERC4626Coupon is ERC4626SuiteContext {
     constructor() {
     }
 
-    // use this in totalAssets() instead of asset.balanceOf(this)
-    function availableAssets() public virtual override view returns (uint256 avail) {
-        avail = super.availableAssets();
-        assert(avail >= couponAssetsReserved); // should never get here
-        unchecked {
-            avail -= couponAssetsReserved;
-        }
-    }
-
     function distributeCoupon(uint256 _coupon) internal virtual onlyManager {
         require(availableAssets() >= _coupon, "distributeCoupon: not enough assets");
         totalCouponPaid += _coupon;
@@ -48,6 +37,13 @@ abstract contract ERC4626Coupon is ERC4626SuiteContext {
         return _coupons[owner].remainingCoupon + (totalCouponPaid - _coupons[owner].lastTotalCoupon).mulDiv(balanceOf(owner), totSup);
     }
 
+    function updateCouponBalance(address owner) public virtual returns (uint256) {
+        uint256 totSup = totalSupply();
+        if (totSup == 0) return 0;
+        _coupons[owner].lastTotalCoupon = totalCouponPaid;
+        return _coupons[owner].remainingCoupon = couponBalance(owner);
+    }
+
     function claimCoupon (address owner, address to, uint256 amount) public virtual {
         uint256 avail = couponBalance(owner);
         if (amount > avail) amount = avail;
@@ -56,4 +52,25 @@ abstract contract ERC4626Coupon is ERC4626SuiteContext {
         couponAssetsReserved -= amount;
         require(IERC20(asset()).transfer(to, amount), "claimCoupon: Transfer failed");
     }
+
+    // use this in totalAssets() instead of asset.balanceOf(this)
+    function availableAssets() public virtual override view returns (uint256 avail) {
+        avail = super.availableAssets();
+        assert(avail >= couponAssetsReserved); // should never get here
+        unchecked {
+            avail -= couponAssetsReserved;
+        }
+    }
+
+    // be sure to update coupon balances before changing underlying token balances
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        if (to != address(0)) updateCouponBalance(to);
+        if (from != address(0)) updateCouponBalance(from);
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
 }
