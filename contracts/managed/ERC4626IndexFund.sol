3d1aa0dbc6f50e6869553f3e273076068fe8ebe8
Tom Shields <tom@eriven.com>
2022-09-12 17:22:45 -0700

    Index Fund is a lot simpler now

--- contracts/managed/ERC4626IndexFund.sol
+++ contracts/managed/ERC4626IndexFund.sol
@@ -5,39 +5,14 @@ pragma solidity >=0.7.0 <0.9.0;
 import "../fees/ERC4626Fee.sol";
 import "../access/ERC4626Access.sol";
 import "../access/ERC4626Force.sol";
+import "./ERC4626AssetVault.sol";
+import "./ERC4626AssetOffChain.sol";
+
 
 /// @notice Index Fund vault for ERC4626 investments. Manager decides when to invest/divest in any ERC4626 vault.
 /// @notice Gains are not computed until investments are updated, either by an investment, or a call to updateAssetValue.
 
-contract ERC4626IndexFund is ERC4626Fee, ERC4626Access, ERC4626Force {
-
-/*
-Key things to think about:
-If underlying asset values change dramatically, there will be an arb opportunity before they
-get updated here. Not sure how to handle that. Can do more frequent updating with round-robin
-but the problem still remains.
-*/
-/* Potential optimization to add:
-   uint256 nextInvestmentToUpdate; // every tx updates at least one other investment too, spread the cost
-   uint256 updateQuantum = 1000; // don't update if already updated this recently
-   function updateNextAssetValue() - call from every tx to update next in line
-*/
-
-    // Events
-    event depositInvestmentEvent(address indexed vault, uint256 amount);
-    event redeemInvestmentEvent(address indexed vault, uint256 amount);
-
-    // Constants
-    uint8 constant MAX_INVESTMENTS = 255;
-
-    // Variables
-    struct investment {
-        ERC4626 vault;
-        uint256 lastAssets;
-    }
-    investment[] investments; // keep track of all the investments
-    mapping(address => uint32) public investmentIndex; // so we can look them up easily
-    uint256 investmentAssetsTotal;
+contract ERC4626IndexFund is ERC4626AssetVault, ERC4626AssetOffChain, ERC4626Fee, ERC4626Access, ERC4626Force {
 
     constructor(
         ERC20 _asset,
@@ -53,62 +28,14 @@ but the problem still remains.
     {
     }
 
-    // Investment manager functions
-
-    // used in totalAssets()
-    function totalNAV () public view virtual override returns (uint256) {
-        return investmentAssetsTotal;
-    }
-    
-    function depositInvestment(ERC4626 _vault, uint256 _assets) public virtual onlyManager returns (uint256 _shares) {
-        require(_vault.asset() == asset(), "Investment asset does not match vault");
-
-        uint32 index = investmentIndex[address(_vault)];
-        if (investments[index].vault != _vault) { // new investment vault, index was zero
-            index = uint32(investments.length);
-            require(index < MAX_INVESTMENTS, "Investment would exceed MAXINVESTMENTS");
-            investmentIndex[address(_vault)] = index;
-            investments.push(investment(_vault, 0));
-        }
-
-        _shares = _vault.deposit(_assets, address(this));
-        updateAssetValueIndex(index);
-        emit depositInvestmentEvent(address(_vault), _assets);
-    }
-
-    function redeemInvestment(ERC4626 _vault, uint256 _shares) public virtual onlyManager returns (uint256 _assets) {
-        uint32 index = investmentIndex[address(_vault)];
-        uint256 sharesMax = _vault.maxWithdraw(address(this));
-        if (_shares > sharesMax) _shares = sharesMax;
-        _assets = _vault.redeem(_shares, address(this), address(this));
-        updateAssetValueIndex(index);
-        emit redeemInvestmentEvent(address(_vault), _shares);
-    }
-
-    function updateAssetValueIndex(uint32 index) internal virtual returns (uint256 assets) {
-        require(index < investments.length, "Index out of range");
-        assets = investments[index].vault.maxWithdraw(address(this));  // won't take into account all fees, but close enough
-        investmentAssetsTotal += assets - investments[index].lastAssets;
-        investments[index].lastAssets = assets;
-    }
-
-    // anyone can update a single vault asset value
-    function updateAssetValue(ERC4626 _vault) public virtual returns (uint256) {
-        return updateAssetValueIndex(investmentIndex[address(_vault)]);
-    }
-
-    // probably costs a lot of gas, but here just in case a full reset is needed
-    function updateAllAssets() public virtual returns (uint256) {
-        investmentAssetsTotal = 0;
-        for(uint8 i=0; i<= investments.length; i++){
-            uint256 assetVal = investments[i].vault.maxWithdraw(address(this));
-            investments[i].lastAssets = assetVal;
-            investmentAssetsTotal += assetVal;
-        }
-        return investmentAssetsTotal;
-    }
+    // interesting - all the logic is in the components
+    function distributeCoupon(uint256 _coupon) public virtual override {}
 
     // Everything below here is just crap to satisfy the compiler about multiple inheritance
+    function totalNAV() public virtual override(ERC4626AssetBase, ERC4626SuiteContext) view returns (uint256) {
+        return super.totalNAV();
+    }
+    
     function requireManager() internal view override(ERC4626Access, ERC4626SuiteContext) {
       return super.requireManager();
     }
@@ -158,6 +85,9 @@ but the problem still remains.
     }
     function maxRedeem(address owner) public view virtual override(ERC4626, ERC4626Access) returns (uint256) {
         return super.maxRedeem(owner);
+    }
+    function getLatestNAV(assetStruct storage asset) internal view virtual override(ERC4626AssetBase, ERC4626AssetVault) returns (uint256) {
+        return super.getLatestNAV(asset);
     }
 
 }
