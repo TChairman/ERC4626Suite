eae9574ed52c8efc33cb029c815cc6add26a391a
Tom Shields <tom@eriven.com>
2022-09-25 22:53:48 -0700

    enforce deposits only from committed investors

--- contracts/equity/ERC4626CapitalCall.sol
+++ contracts/equity/ERC4626CapitalCall.sol
@@ -21,16 +21,17 @@ abstract contract ERC4626CapitalCall is ERC4626DepositEscrow {
         address receiver; // wallet where shares are deposited when capital calls are finalized
     }
     investorType[] public _investors;
-    mapping (bytes32 => uint32) public _investorIndex;
+    mapping (bytes32 => uint32) public _investorByName;
+    mapping (address => uint32) private _investorByAddress;
 
     constructor() {
         _investors.push(investorType("", 0, 0, address(0), address(0))); // push guard entry
     }
 
     function addInvestor(bytes32 _id, uint256 _commitment, address _depositor) public virtual onlyManager {
-        require(_investorIndex[_id] == 0, "addInvestor: investor already exists");
+        require(_investorByName[_id] == 0, "addInvestor: investor already exists");
         require(_investors.length < MAX_INVESTORS, "addInvestor: max investors reached");
-        _investorIndex[_id] = uint32(_investors.length);
+        _investorByAddress[_depositor] = _investorByName[_id] = uint32(_investors.length);
         _investors.push(investorType(_id, _commitment, 0, _depositor, _depositor));
     }
 
@@ -42,17 +43,19 @@ abstract contract ERC4626CapitalCall is ERC4626DepositEscrow {
     }
 
     function setInvestorCommitment(bytes32 _id, uint256 _commitment) public virtual onlyManager {
-        uint32 index = _investorIndex[_id];
+        uint32 index = _investorByName[_id];
         require(index > 0, "investor does not exist");
         _investors[index].commitment = _commitment;
     }
     function setInvestorDepositor(bytes32 _id, address _depositor) public virtual onlyManager {
-        uint32 index = _investorIndex[_id];
+        uint32 index = _investorByName[_id];
         require(index > 0, "investor does not exist");
+        _investorByAddress[_investors[index].depositor] = 0;
         _investors[index].depositor = _depositor;
+        _investorByAddress[_depositor] = index;
     }
     function setInvestorReceiver(bytes32 _id, address _receiver) public virtual onlyManager {
-        uint32 index = _investorIndex[_id];
+        uint32 index = _investorByName[_id];
         require(index > 0, "investor does not exist");
         _investors[index].receiver = _receiver;
     }
@@ -65,7 +68,7 @@ abstract contract ERC4626CapitalCall is ERC4626DepositEscrow {
     }
 
     function getDelinquentAmount(bytes32 _id) public virtual returns (uint256) {
-        return getDelinquentAmountIndex(_investorIndex[_id]);
+        return getDelinquentAmountIndex(_investorByName[_id]);
     }
 
     function getDelinquentAmountIndex(uint32 _index) public virtual returns (uint256) {
@@ -88,4 +91,14 @@ abstract contract ERC4626CapitalCall is ERC4626DepositEscrow {
         }
     }
 
+    // deposits only allowed from investors that have commitments set
+    function deposit(uint256 assets, address receiver) public virtual override returns (uint256) {
+        require(_investorByAddress[_msgSender()] > 0, "deposit: not in capital call list");
+        return super.deposit(assets, receiver);
+    }
+
+    // override because the capital call check is sufficient
+    function isDepositMintAllowed(address) public view virtual override returns (bool) {
+        return true;
+    }
 }
